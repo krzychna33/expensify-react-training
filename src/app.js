7d641df624febdd2b3e0013ac25fc92eaf88479a
krzychna33 <krzychna33@gmail.com>
2018-11-07 15:03:22 +0100

    app.js fixes

--- src/app.js
+++ src/app.js
@@ -8,37 +8,7 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 
-import { editExpense, addExpense, removeExpense } from "./actions/expenses";
-import { setTextFilter, sortByAmount } from "./actions/filters";
-import getVisibleExpenses from "./selectors/expenses";
-
 const store = configureStore();
-
-
-store.dispatch(addExpense({
-    description: 'Water Bill',
-    amount: 1000
-}));
-
-store.dispatch(addExpense({
-    description: "Gas Bill",
-    amount: 997
-}));
-
-store.dispatch(addExpense({
-    description: 'Rent',
-    amount: 240000
-}));
-
-// store.dispatch(setTextFilter('bill'));
-// store.dispatch(sortByAmount());
-
-// setTimeout(()=>{
-//     store.dispatch(setTextFilter('rent'));
-// }, 3000)
-
-const state = store.getState();
-console.log(getVisibleExpenses(state.expenses, state.filters));
 
 const jsx = (
     <Provider store={store}>
